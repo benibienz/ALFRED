65f4415bd4aea9d565796797e0b93569ffffb430
benibienz <benibienz@gmail.com>
2020-04-07 15:43:34 -0600

    simplify app by removing "factory" setup

--- MVP/main.py
+++ MVP/main.py
@@ -1,3 +1,6 @@
-""" To make Google App Engine work easier """
-import os
-os.system('sh launch_demo.sh')
+from flask import Flask
+from core import gui
+
+app = Flask(__name__)
+app.register_blueprint(gui.bp)
+app.run(host='127.0.0.1', port=8080, debug=True)
