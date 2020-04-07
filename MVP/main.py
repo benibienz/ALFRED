9c477135098fd5ecaefa0b3a8811be0d64b32555
benibienz <benibienz@gmail.com>
2020-04-07 16:07:58 -0600

    guard app.run()

--- MVP/main.py
+++ MVP/main.py
@@ -3,4 +3,6 @@ from core import gui
 
 app = Flask(__name__)
 app.register_blueprint(gui.bp)
-app.run(host='127.0.0.1', port=5000, debug=True)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
